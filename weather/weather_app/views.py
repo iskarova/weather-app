55857c43c97d9c094a48d05dd700d21dac8f4e98
lina iskarova <cyberdeadushka@gmail.com>
2025-05-23 14:22:12 +0300

    unput only for strings

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -1,4 +1,5 @@
 import requests
+import string
 from django.shortcuts import render
 
 
@@ -11,6 +12,11 @@ def index(request):
   if request.method == "POST":
     first_city = request.POST["first_city"]
     second_city = request.POST.get("second_city", None)
+    
+    if valid_city(first_city) is False:
+      first_city = None
+    if valid_city(second_city) is False:
+      second_city = None
     
     if first_city and second_city:
       weather_data_1 = fetch_info(first_city, API_KEY, current_weather_url)
@@ -45,3 +51,7 @@ def fetch_info(city, api_key, current_weather_url):
   }
   
   return weather_data
+
+def valid_city(any_city):
+  check_str = string.ascii_letters + '-'
+  return all(char in check_str for char in any_city)
